6e033c48e620e1b8111d7d9cf2ba64e554f6a568
michael ozeri <ozeri.mich@gmai.com>
2018-11-26 21:35:48 +0200

    - added TODO - added comments for functions - added global to update eij array or not at each iteration

--- EM-MMM.py
+++ EM-MMM.py
@@ -1,6 +1,5 @@
 import json
 import numpy as np
-import pprint as pp
 from MMM import MMM
 
 # read example data from JSON
@@ -14,16 +13,15 @@ input_x = data['input']
 with open('data/ICGC-BRCA.json') as f1:
     dic_data = json.load(f1)
 
-# TODO: finish parsing the ICGC file
+# TODO: finish parsing the ICGC file - i guess this is the data to run on, what should i use here?
 
 # read signatures array from BRCA-signatures.npy
-# this is an array of 12x96 - [i,j] is e_ij
+# this is an array of 12x96 - [i,j] is e_ij - fixed in this case until we change
 signatures_data = np.load("data/BRCA-signatures.npy")
 
-mmm = MMM(signatures_data,initial_pi,input_x)
+mmm = MMM(signatures_data, initial_pi, input_x)
 
+mmm.fit(input_x, 0.01, 200)
 
-mmm.fit(input_x,0.01,200)
-
-
-
+# TODO: finish this function
+print(mmm.likelihood(dic_data))
