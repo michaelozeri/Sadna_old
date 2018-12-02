4e2c6fa5fdf32c0e74a7b093cde2c8a34002af64
michael ozeri <ozeri.mich@gmai.com>
2018-12-02 23:55:39 +0200

    unit Testing check

---
+++ test_MMM.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestMMM(TestCase):
+    def test_fit(self):
+        self.fail()
+
+    def test_e_step(self):
+        self.fail()
+
+    def test_likelihood(self):
+        self.fail()
+
+    def test_m_step(self):
+        self.fail()
+
+    def test_convert_to_log_scale(self):
+        self.fail()
+
+    def test_create_b_array(self):
+        self.fail()
+
+    def test_log_to_regular(self):
+        self.fail()
